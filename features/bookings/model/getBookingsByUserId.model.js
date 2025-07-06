6f30b737b00fb44175ecc372b4048ee6faf58ef0
myhzy <notmyhzy@hotmail.com>
2025-07-06 17:05:26 +0100

    implemented ORDER BY created_at DESC in getBookings model

--- features/bookings/model/getBookingsByUserId.model.js
+++ features/bookings/model/getBookingsByUserId.model.js
@@ -4,7 +4,7 @@ exports.getBookingsByUserId = async (user_id) => {
   const query = `
     SELECT * FROM bookings
     WHERE guest_id = $1
-    ORDER BY check_in_date ASC;
+    ORDER BY created_at DESC;
   `;
 
   const values = [user_id];

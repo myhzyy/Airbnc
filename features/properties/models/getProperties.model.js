ac450dcf4af0cec88f0932e9f82474d6a074607b
myhzy <notmyhzy@hotmail.com>
2025-07-29 14:27:53 +0100

    Include lat/lng in fetchProperties for all environments

--- features/properties/models/getProperties.model.js
+++ features/properties/models/getProperties.model.js
@@ -1,8 +1,6 @@
 const format = require("pg-format");
 const db = require("../../../db/connections/dbConnectionPool");
 
-const ENV = process.env.NODE_ENV || "production";
-
 exports.fetchProperties = async (queryParams) => {
   const { maxprice, minprice, sort, order, host, amenity } = queryParams;
 
@@ -34,7 +32,8 @@ exports.fetchProperties = async (queryParams) => {
       properties.name AS property_name,
       properties.location,
       properties.price_per_night,
-      ${ENV === "dev" ? "properties.latitude, properties.longitude," : ""}
+      properties.latitude,
+      properties.longitude,
       CONCAT(users.first_name, ' ', users.surname) AS host,
       COUNT(favourites.favourite_id) AS favourite_count,
       img.image_url AS image

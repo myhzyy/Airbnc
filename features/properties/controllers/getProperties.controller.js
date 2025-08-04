11b7c3dac3f48a7152710355d2a7f1a66f24f456
myhzy <notmyhzy@hotmail.com>
2025-08-04 11:49:05 +0100

    Debug: add logs inside getProperties controller

--- features/properties/controllers/getProperties.controller.js
+++ features/properties/controllers/getProperties.controller.js
@@ -1,8 +1,12 @@
+console.log("⚙️ getProperties controller loaded");
+
 const { fetchProperties } = require("../models/getProperties.model");
 
 exports.getProperties = async (req, res, next) => {
   try {
+    console.log("🧪 Calling fetchProperties with:", req.query);
     const properties = await fetchProperties(req.query);
+    console.log("✅ Properties returned:", properties.length);
     res.status(200).send({ properties });
   } catch (err) {
     console.error("❌ Error in getProperties:", err);
